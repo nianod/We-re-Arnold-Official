44b19b18cf06ba2d6385ba598645cc7e47af0e78
nianod <arnoldkk422@gmail.com>
2025-08-07 21:45:22 +0300

    progress

--- src/Components/Connect.jsx
+++ src/Components/Connect.jsx
@@ -12,24 +12,30 @@ const contactSites = [
 
 const Connect = () => {
   return (
-    <div className="mt-20">
-        <h1 className="text-center text-2xl font-bold text-green-600">Connect with Us</h1>
-        <h2 className="text-center text-[15px] font-semibold text-gray-400 mt-4">Follow, Connect & Reach to us</h2>
-    <div className="text-white flex items-center justify-center gap-2 mt-7">
-      {contactSites.map((contact, index) => (
-        <div key={index}
+    <div className="contact mt-20 border-gray-500 border-t border-b p-6">
+      <h1 className="text-center text-2xl font-bold text-green-600">
+        Connect with Us
+      </h1>
+      <h2 className="text-center text-[15px] font-semibold text-gray-400 mt-4">
+        Follow, Connect & Reach to us
+      </h2>
+      <div className="text-white flex items-center justify-center gap-2 mt-7">
+        {contactSites.map((contact, index) => (
+          <div
+            key={index}
             className="border justify-center flex gap-2 p-2 rounded-xl h-20 max-w-[70px]"
-        >
-            <a href={contact.Url} target="_blank">
-                <p className="flex justify-center flex-col items-center text-2xl">{contact.icon}
-                    <span className="text-[15px] mt-2">{contact.label}</span>
-                </p>
+          >
+            <a href={contact.Url} target="_blank" rel="noopener noreferrer">
+              <p className="flex justify-center flex-col items-center text-2xl">
+                {contact.icon}
+                <span className="text-[15px] mt-2">{contact.label}</span>
+              </p>
             </a>
-        </div>
-      ))}
+          </div>
+        ))}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
 export default Connect
