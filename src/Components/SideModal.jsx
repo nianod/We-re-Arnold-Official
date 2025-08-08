d29020ca5daac08d77dbc6f983cf67953bbde431
nianod <arnoldkk422@gmail.com>
2025-08-08 17:21:55 +0300

    full modal

--- src/Components/SideModal.jsx
+++ src/Components/SideModal.jsx
@@ -1,27 +1,27 @@
-
+import { Link } from 'react-router-dom'
 const SideModal = ({sideMenu, setSideMenu}) => {
   
   return (
     <div>  
       {sideMenu && (
         <div
-          className="fixed inset-0 bg-opacity-40 z-40"
+          className="fixed inset-0  z-40 backdrop-blur-md"
           onClick={() => setSideMenu(false)}
         ></div>
       )}
       <div
-        className={`fixed top-0 right-0 h-screen w-[30%] bg-green-500 z-50 transform transition-transform duration-300 ${
+        className={`fixed top-0 right-0 h-screen w-[30%] bg-blue-950 z-50 transform transition-transform duration-300 ${
           sideMenu ? "translate-x-0" : "translate-x-full"
         }`}
       >
         <div className="p-4 flex justify-between items-center">
-          <h2 className="text-white text-lg">Menu</h2>
-          <button onClick={() => setSideMenu(false)} className="text-gray-800 font-bold cursor-pointer">
+          <h2 className="text-green-500 text-xl font-[helvetica]">Menu</h2>
+          <button onClick={() => setSideMenu(false)} className="text-gray-300 font-bold cursor-pointer">
             ╳
           </button>
         </div>
         <div className="p-4 text-white">
-          <p></p>
+          <p><Link to="/">🏠︎ Home</Link></p>
         </div>
       </div>
     </div>
