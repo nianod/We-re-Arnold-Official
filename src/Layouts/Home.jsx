a37858c9065b02afa42210e05b04e8806733e487
nianod <arnoldkk422@gmail.com>
2025-08-09 22:55:39 +0300

    today's commit

--- src/Layouts/Home.jsx
+++ src/Layouts/Home.jsx
@@ -1,34 +1,44 @@
 import React from 'react'
 import Connect from '../Components/Connect'
 import SideModal from '../Components/SideModal'
-import { motion, useScroll } from 'framer-motion'
+import { motion, useScroll, useSpring } from 'framer-motion'
 
 const Home = () => {
   const { scrollYProgress } = useScroll()
+  const scaleX = useSpring(scrollYProgress, { stiffness: 100, damping: 30, restDelta: 0.001 })
 
+   
   return (
     <div className='mt-20'>
       <motion.div
         id='scroll-indicator'
         style={{
-          scaleX: scrollYProgress,
-          top: 0,
-          position: 'fixed',
+          scaleX: scaleX,
+          top: 64,
+          position: "fixed",
           left: 0,
           right: 0,
-          height: 15,
+          height: 5,
           originX: 0,
           background: '#ff0088',
           zIndex: 10
         }}
-      />
+      ></motion.div>
       <div>
-        <h1 className='text-white text-xl text-center'>
-          WELCOME TO
-          <span className='text-white text-xl bg-[red] rounded shadow-md font-bold'>
-            {" "}We're Arnold's Tech
-          </span>
+        <h1 className='text-white text-xl text-center font-bold'>HEY, {" "}
+          <motion.span className='text-white text-xl rounded shadow-md font-bold clips'
+            initial={{ y: -100, opacity: 0}}
+            animate={{ y: 0, opacity: 1}}
+            transition={{ duration: 0.8, ease: 'easeOut'}}
+          >{" "} We're Arnold's Tech
+          </motion.span>
         </h1>
+        <p className='text-gray-300 text-center'>
+          Your All-in-One Digital Solutions Partner <br />
+          <span>
+            Software Development | Web Design | Social Media Growth & More
+          </span>
+        </p>
       </div>
       <Connect />
       <Connect />
