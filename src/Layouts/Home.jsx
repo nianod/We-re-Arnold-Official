dce9c67cb7762a14938d07171351186d3cc6b47c
nianod <arnoldkk422@gmail.com>
2025-08-09 21:54:24 +0300

    today's commit

--- src/Layouts/Home.jsx
+++ src/Layouts/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import Connect from '../Components/Connect'
 import SideModal from '../Components/SideModal'
+import { motion, useScroll } from 'motion/react'
 
 const Home = () => {
+  const { scrollyProgress } = useScroll()
+   
   return (
-    <div>
+    <div className='mt-15'>
+      <motion.div
+        id='scroll indicator'
+        style={{
+          scaleX: scrollyProgress,
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 5,
+          originX: 0,
+          background: 'ff0088'
+        }}
+      ></motion.div>
+      <div>
+        <h1 className='text-white'>WELCOME TO <span>We're Arnold's Tech</span></h1>
+
+      </div>
       <Connect />
       <SideModal />
     </div>
