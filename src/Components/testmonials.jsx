90c57666624124098110a55a088a20d24d2fd81f
nianod <arnoldkk422@gmail.com>
2025-08-25 19:07:34 +0300

    testimoniols

--- src/Components/testmonials.jsx
+++ src/Components/testmonials.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 
 const testmonials = () => {
+    const testimonies = [
+        {
+            name: "John Doe",
+            residantion: "Phillipines",
+            image: "/download (2).jpg",
+            quote: "I've worked with Arnold and I have recognixed their potential in whatever they do",
+            rank: "CEO Glucose company"
+        }
+    ]
   return (
     <div>
       
@@ -9,3 +18,4 @@ const testmonials = () => {
 }
 
 export default testmonials
+ 
